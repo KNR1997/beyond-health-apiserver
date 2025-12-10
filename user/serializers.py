a42469309f9da0adc8d6fa22b51361e81958abbc
Ayeshika17 <ayeshikadilrukshiperera@gmail.com>
2025-12-10 18:27:15 +0530

    feat(patient): update dentist details handling

--- user/serializers.py
+++ user/serializers.py
@@ -196,6 +196,66 @@ class DentistCreateSerializer(serializers.ModelSerializer):
         return dentist
 
 
+class DentistUpdateSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(write_only=True, required=False)
+    last_name = serializers.CharField(write_only=True, required=False)
+    username = serializers.CharField(write_only=True, required=False)
+    email = serializers.EmailField(write_only=True, required=False)
+
+    class Meta:
+        model = Dentist
+        fields = "__all__"
+        read_only_fields = ["user"]
+
+    # --- VALIDATION ---
+    def validate_username(self, value):
+        user = self.instance.user
+        if User.objects.filter(username=value).exclude(id=user.id).exists():
+            raise serializers.ValidationError("User with this username already exists.")
+        return value
+
+    def validate_email(self, value):
+        user = self.instance.user
+        if User.objects.filter(email=value).exclude(id=user.id).exists():
+            raise serializers.ValidationError("User with this email already exists.")
+        return value
+
+    # --- UPDATE ---
+    def update(self, instance, validated_data):
+        """
+        instance → Patient instance
+        instance.user → related User instance
+        validated_data → student fields + username/email/password
+        """
+
+        user = instance.user
+
+        # Extract user fields
+        first_name = validated_data.pop("first_name", None)
+        last_name = validated_data.pop("last_name", None)
+        username = validated_data.pop("username", None)
+        email = validated_data.pop("email", None)
+
+        # Update User fields
+        if first_name:
+            user.first_name = first_name
+        if last_name:
+            user.last_name = last_name
+        if username:
+            user.username = username
+        if email:
+            user.email = email
+        user.save()
+
+        # Update Patient fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
+
+
 class DentistListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dentist
