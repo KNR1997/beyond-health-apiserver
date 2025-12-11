a58708a029ddc0aa40ed1d1f6959224d8140853b
Ayeshika17 <ayeshikadilrukshiperera@gmail.com>
2025-12-11 07:43:20 +0530

    feat(patient): update dentist details handling

--- user/views/dentist.py
+++ user/views/dentist.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from core.views.base import BaseViewSet
 
 from user.models import Dentist
-from user.serializers import DentistListSerializer, DentistCreateSerializer
+from user.serializers import DentistListSerializer, DentistCreateSerializer, DentistUpdateSerializer
 
 
 # Create your views here.
@@ -28,6 +28,21 @@ class DentistViewSet(BaseViewSet):
 
         output = DentistCreateSerializer(dentist, context={"request": request}).data
         return Response(output, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        dentist = Dentist.objects.get(pk=kwargs["pk"])
+
+        serializer = DentistUpdateSerializer(
+            dentist,
+            data=request.data,
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        dentist = serializer.save()
+
+        output = DentistListSerializer(dentist, context={"request": request}).data
+        return Response(output, status=status.HTTP_200_OK)
+
 
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
