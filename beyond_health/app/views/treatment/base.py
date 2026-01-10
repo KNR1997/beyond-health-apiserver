285794a0bb902960b4c690fc1df4d72f019e9300
KNR1997 <kethakaranasinghe@gmail.com>
2026-01-10 15:17:00 +0530

    feat: treatment-crud

---
+++ beyond_health/app/views/treatment/base.py
@@ -0,0 +1,32 @@
+from beyond_health.app.base import BaseViewSet
+from beyond_health.app.serializers.treatment import TreatmentListSerializer
+from beyond_health.db.models import Treatment
+
+
+# Create your views here.
+class TreatmentViewSet(BaseViewSet):
+    model = Treatment
+    serializer_class = TreatmentListSerializer
+
+    search_fields = ["name"]
+    filterset_fields = []
+
+    def get_queryset(self):
+        return (
+            self.filter_queryset(super().get_queryset())
+        )
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
