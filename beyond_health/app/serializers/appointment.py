0d7855583fc4f5228a3c038801406f5bab5241dc
KNR1997 <kethakaranasinghe@gmail.com>
2026-07-22 14:13:55 +0530

    feat: Appointment crud

--- beyond_health/app/serializers/appointment.py
+++ beyond_health/app/serializers/appointment.py
@@ -1,16 +1,26 @@
 from beyond_health.app.serializers.base import BaseSerializer
 from beyond_health.db.models.appointment import Appointment
+from beyond_health.app.serializers.patient import PatientListSerializer
+from beyond_health.app.serializers.dentist import DentistListSerializer
 
 
 class AppointmentListSerializer(BaseSerializer):
+    patient = PatientListSerializer()
+    dentist = DentistListSerializer()
+
     class Meta:
         model = Appointment
         fields = [
             'id',
+            'appointment_no',
             'appointment_date',
             'duration',
             'reason_for_visit',
-            'status'
+            'appointment_type',
+            'status',
+
+            'patient',
+            'dentist',
         ]
 
 
@@ -19,6 +29,7 @@ class AppointmentCreateSerializer(BaseSerializer):
         model = Appointment
         fields = [
             'patient',
+            'dentist',
             'appointment_date',
             'appointment_type',
         ]
@@ -29,9 +40,7 @@ class AppointmentUpdateSerializer(BaseSerializer):
         model = Appointment
         fields = [
             'patient',
+            'dentist',
             'appointment_date',
             'appointment_type',
-            'reason_for_visit',
-            'status',
-            'notes',
         ]
